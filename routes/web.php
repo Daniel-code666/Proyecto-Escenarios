290defe4caf8d68490b9240d4e43999b3c6e58d8
DanielMoraUDEC <55363173+DanielMoraUDEC@users.noreply.github.com>
2022-04-15 19:31:32 -0500

    Update web.php
    prueba1

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//hello
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
