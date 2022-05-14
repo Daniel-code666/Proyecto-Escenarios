47e4c602388f2805939d84323429c5ba9f65aa19
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2022-05-14 16:31:51 -0500

    Ubica la lat y lng en los cuadros de texto

--- public/argon/js/map.js
+++ public/argon/js/map.js
@@ -5,39 +5,47 @@ var $map = $('#map-default'),
     color = "#5e72e4";
 
 function initMap() {
-
     map = document.getElementById('map-default');
-    lat = map.getAttribute('data-lat');
-    lng = map.getAttribute('data-lng');
+    // lat = map.getAttribute('data-lat');
+    lat = 4.60971;
+    // lng = map.getAttribute('data-lng');
+    lng = -74.08175;
 
-    var myLatlng = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
+    map = new google.maps.Map(document.getElementById('map-default'), {
         zoom: 12,
         scrollwheel: true,
-        center: myLatlng,
+        center: new google.maps.LatLng(4.60971, -74.08175),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-    }
-
-    map = new google.maps.Map(map, mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        title: 'Hello World!'
     });
 
-    var contentString = marker.getPosition().lat();
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(4.60971, -74.08175),
+        map: map,
+        animation: google.maps.Animation.DROP,
+        // title: 'Marcador',
+        draggable: true
+    });
 
     var infowindow = new google.maps.InfoWindow({
         content: String(marker.getPosition())
     });
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
+    google.maps.event.addListener(marker, 'click', function(evt) {
+        infowindow.open(map, marker); 
     });
+
+    google.maps.event.addListener(marker, 'dragend', function(evt){
+        $("#lat").val(evt.latLng.lat().toFixed(6));
+        $("#lng").val(evt.latLng.lng().toFixed(6));
+
+        map.panTo(evt.latLng);
+    });
+
+    map.setCenter(marker.position);
+
+    marker.setMap(map);
 }
 
 if($map.length) {
     google.maps.event.addDomListener(window, 'load', initMap);
-}
+}   
