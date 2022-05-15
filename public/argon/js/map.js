63b8045e902e0e8c736ca9c967f97b581583578c
Daniel Mora <djosemora@ucundinamarca.edu.co>
2022-05-15 12:56:30 -0500

    map

--- public/argon/js/map.js
+++ public/argon/js/map.js
@@ -4,39 +4,47 @@ var $map = $('#map-default'),
     lng,
     color = "#5e72e4";
 
-function initMap() {
-
-    map = document.getElementById('map-default');
-    lat = map.getAttribute('data-lat');
-    lng = map.getAttribute('data-lng');
-
-    var myLatlng = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
-        zoom: 12,
-        scrollwheel: true,
-        center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
+    function initMap() {
+        map = document.getElementById('map-default');
+        // lat = map.getAttribute('data-lat');
+        lat = 4.60971;
+        // lng = map.getAttribute('data-lng');
+        lng = -74.08175;
+    
+        map = new google.maps.Map(document.getElementById('map-default'), {
+            zoom: 12,
+            scrollwheel: true,
+            center: new google.maps.LatLng(4.60971, -74.08175),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+        });
+    
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(4.60971, -74.08175),
+            map: map,
+            animation: google.maps.Animation.DROP,
+            // title: 'Marcador',
+            draggable: true
+        });
+    
+        var infowindow = new google.maps.InfoWindow({
+            content: String(marker.getPosition())
+        });
+    
+        google.maps.event.addListener(marker, 'click', function(evt) {
+            infowindow.open(map, marker); 
+        });
+    
+        google.maps.event.addListener(marker, 'dragend', function(evt){
+            $("#lat").val(evt.latLng.lat().toFixed(6));
+            $("#lng").val(evt.latLng.lng().toFixed(6));
+    
+            map.panTo(evt.latLng);
+        });
+    
+        map.setCenter(marker.position);
+    
+        marker.setMap(map);
     }
-
-    map = new google.maps.Map(map, mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        title: 'Hello World!'
-    });
-
-    var contentString = marker.getPosition().lat();
-
-    var infowindow = new google.maps.InfoWindow({
-        content: String(marker.getPosition())
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
-    });
-}
 
 if($map.length) {
     google.maps.event.addDomListener(window, 'load', initMap);
