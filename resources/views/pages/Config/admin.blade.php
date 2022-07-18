9cbeb7fa44fd840bd5568cc33b98d370208d4fd4
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2022-07-18 14:39:11 -0500

    Datatables para estados

--- resources/views/pages/Config/admin.blade.php
+++ resources/views/pages/Config/admin.blade.php
@@ -1,43 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('layouts.headers.sharedmargin')
+@include('layouts.headers.sharedmargin')
 
-    <H2 class="text-center fw-bold mt-4" style="font-size: 30px">Configuraciones</H2>
-    <br>
-    <hr>
-    <div class="row m-2">
-        <div class="col-md-4">
-            <div class="card" style="width: 100%;">
-                <div class="card-body">
-                  <h5 class="card-title text-center" style="font-size: 20px">Parametrizaciones</h5>
-                    <hr>
-                  <ul style="list-style: none">
-                      <li><a href="{{url('/discipline')}}" class=""><i class="ni ni-user-run text-purple"></i> Disciplinas</a></li>
-                      <li><a href="{{url('/states')}}" class=""><i class="ni ni-bullet-list-67 text-purple"></i> Estado de los escenarios</a></li>
-                      <li><a href="{{url('/inventarystates')}}" class=""><i class="ni ni-bullet-list-67 text-purple"></i> Estado de los inventarios</a></li>
-                  </ul> 
-                </div>
-             </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                  <h5 class="card-title text-center" style="font-size: 20px">Otros</h5>
-                    <hr>
-                  <ul>
-                      <li><a href="{{url('/discipline')}}" class="">Disciplinas</a></li>
-                      <li><a href="{{url('/escenario')}}" class="">Estado de los escenarios</a></li>
-                      <li><a href="{{url('/inventarystates')}}" class="">Estado de los inventarios</a></li>
-                  </ul> 
-                </div>
-              </div>
-        </div>
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
 
+<h2 class="text-center fw-bold mt-4" style="font-size: 30px">Configuraciones</h2>
+<br>
+<hr>
+<div class="row m-2">
+  <div class="col-md-4">
+    <div class="card" style="width: 100%;">
+      <div class="card-body">
+        <h5 class="card-title text-center" style="font-size: 20px">Parametrizaciones</h5>
+        <hr>
+        <ul style="list-style: none">
+          <li><a href="{{url('/discipline')}}" class=""><i class="ni ni-user-run text-purple"></i> Disciplinas</a></li>
+          <li><a href="{{url('/states')}}" class=""><i class="ni ni-bullet-list-67 text-purple"></i> Estado de los escenarios</a></li>
+          <li><a href="{{url('/inventarystates')}}" class=""><i class="ni ni-bullet-list-67 text-purple"></i> Estado de los inventarios</a></li>
+        </ul>
+      </div>
     </div>
+  </div>
+  <div class="col-md-4">
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title text-center" style="font-size: 20px">Otros</h5>
+        <hr>
+        <ul>
+          <li><a href="{{url('/discipline')}}" class="">Disciplinas</a></li>
+          <li><a href="{{url('/escenario')}}" class="">Estado de los escenarios</a></li>
+          <li><a href="{{url('/inventarystates')}}" class="">Estado de los inventarios</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 
-   
-    @include('layouts.footers.auth')
+</div>
+
+
+@include('layouts.footers.auth')
 </div>
 @endsection
 
--- resources/views/pages/misclist-Inventary/form.blade.php
+++ resources/views/pages/misclist-Inventary/form.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-3">
         <div class="form-group">
             <label for="example-text-input" class="form-control-label">Nombre del estado</label>
-            <input class="form-control" type="text" name="name" value="{{isset($stateInventary->name)?$stateescenary->name:''}}" required="Este es un campo obligatorio.">
+            <input class="form-control" type="text" name="name" value="{{isset($stateInventary->name)?$stateInventary->name:''}}" required="Este es un campo obligatorio.">
         </div>
 
     </div>
@@ -12,7 +12,7 @@
     <div class="col-md-12">
         <div class="form-group">
             <label for="exampleFormControlTextarea1">Descripción del estado</label>
-            <textarea class="form-control" id="description" rows="2" name="description">{{isset($stateInventary->description)?$stateescenary->description:''}}</textarea>
+            <textarea class="form-control" id="description" rows="2" name="description">{{isset($stateInventary->description)?$stateInventary->description:''}}</textarea>
         </div>
     </div>
 </div>
