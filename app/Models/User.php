96c9158cf5ed9dabeafae94664002926873d2e84
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2022-10-04 13:35:07 -0500

    Fix user model 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
