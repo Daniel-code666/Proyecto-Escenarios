a4627e4c0144e12dfa964318e9cf8cbe7b1354ad
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2022-10-22 20:17:36 -0500

    resorces with state msg understages

--- app/Reports/StageReport.php
+++ app/Reports/StageReport.php
@@ -105,6 +105,17 @@ class StageReport extends \koolreport\KoolReport
                 ->where('misc_list_states.tableParent', '=', 'inventary')
         )->pipe($this->dataStore("resourcesInUse"));
 
+        // query info recursos en uso con mensaje del estado, escenario principal
+        $this->src("mysql")->query(
+            Resources::select('resourceName as Nombre del objeto', 'amountInUse as Cantidad en uso', 'statesName as Estado', 'resourceMsgState as Condición', 'warehouseName as Almacén')
+                ->join('warehouses', 'warehouses.warehouseId', '=', 'resources.resources_warehouseId')
+                ->join('stages', 'stages.id', '=', 'warehouses.warehouseLocation')
+                ->join('misc_list_states', 'misc_list_states.statesId', '=', 'resources.id_category')
+                ->where('stages.id', '=', $this->params["id"])
+                ->where('amountInUse', '>', 0)
+                ->where('misc_list_states.tableParent', '=', 'inventary')
+        )->pipe($this->dataStore("resourcesInUseStateMsg"));
+
         // recursos por almacén en el escenario principal
         $this->src("mysql2")->query(
             "select warehouseName, amount, warehouseId from idrdsystem.resources
@@ -133,8 +144,8 @@ class StageReport extends \koolreport\KoolReport
                 'resourceName as Nombre del objeto',
                 'amount as Cantidad en almacén',
                 'statesName as Estado',
-                'warehouseName as Almacén',
-                'name_understg as Sub escenario'
+                'resourceMsgState as Condición',
+                'warehouseName as Almacén'
             )
                 ->join('misc_list_states', 'misc_list_states.statesId', '=', 'resources.id_category')
                 ->join('warehouses', 'warehouses.warehouseId', '=', 'resources.resources_warehouseId')
@@ -150,6 +161,7 @@ class StageReport extends \koolreport\KoolReport
                 'resourceName as Nombre del objeto',
                 'amountInUse as Cantidad en uso',
                 'statesName as Estado',
+                'resourceMsgState as Condición',
                 'warehouseName as Almacén'
             )
                 ->join('misc_list_states', 'misc_list_states.statesId', '=', 'resources.id_category')
