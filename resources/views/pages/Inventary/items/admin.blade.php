4b58862baf932b5281cef3d63d38e2e7e128d3f5
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2022-10-28 18:29:52 -0500

    Filtro tablas inventario

--- resources/views/pages/Inventary/items/admin.blade.php
+++ resources/views/pages/Inventary/items/admin.blade.php
@@ -111,7 +111,12 @@
             </div>
             <script>
                 $(document).ready(function() {
-                    $('#inventory_table').DataTable({
+                    $('#inventory_table thead tr')
+                        .clone(true)
+                        .addClass('filters')
+                        .appendTo('#inventory_table thead');
+
+                    var table = $('#inventory_table').DataTable({
                         dom: 'Bfrtip',
                         buttons: ['pageLength', 'excelHtml5', 'pdfHtml5'],
                         language: {
@@ -128,6 +133,57 @@
                             buttons: {
                                 pageLength: 'Mostrando %d filas'
                             },
+                        },
+                        orderCellsTop: true,
+                        fixedHeader: true,
+                        initComplete: function() {
+                            var api = this.api();
+
+                            // For each column
+                            api
+                                .columns()
+                                .eq(0)
+                                .each(function(colIdx) {
+                                    // Set the header cell to contain the input element
+                                    var cell = $('.filters th').eq(
+                                        $(api.column(colIdx).header()).index()
+                                    );
+                                    var title = $(cell).text();
+                                    $(cell).html('<input type="text" placeholder="' + title + '" />');
+
+                                    // On every keypress in this input
+                                    $(
+                                            'input',
+                                            $('.filters th').eq($(api.column(colIdx).header()).index())
+                                        )
+                                        .off('keyup change')
+                                        .on('change', function(e) {
+                                            // Get the search value
+                                            $(this).attr('title', $(this).val());
+                                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
+
+                                            var cursorPosition = this.selectionStart;
+                                            // Search the column for that value
+                                            api
+                                                .column(colIdx)
+                                                .search(
+                                                    this.value != '' ?
+                                                    regexr.replace('{search}', '(((' + this.value + ')))') :
+                                                    '',
+                                                    this.value != '',
+                                                    this.value == ''
+                                                )
+                                                .draw();
+                                        })
+                                        .on('keyup', function(e) {
+                                            e.stopPropagation();
+
+                                            $(this).trigger('change');
+                                            $(this)
+                                                .focus()[0]
+                                                .setSelectionRange(cursorPosition, cursorPosition);
+                                        });
+                                });
                         },
                     });
                 });
@@ -208,6 +264,11 @@
             </div>
             <script>
                 $(document).ready(function() {
+                    $('#inventory_table_sub thead tr')
+                        .clone(true)
+                        .addClass('filters')
+                        .appendTo('#inventory_table thead');
+
                     $('#inventory_table_sub').DataTable({
                         dom: 'Bfrtip',
                         buttons: ['pageLength', 'excelHtml5', 'pdfHtml5'],
@@ -225,6 +286,57 @@
                             buttons: {
                                 pageLength: 'Mostrando %d filas'
                             },
+                        },
+                        orderCellsTop: true,
+                        fixedHeader: true,
+                        initComplete: function() {
+                            var api = this.api();
+
+                            // For each column
+                            api
+                                .columns()
+                                .eq(0)
+                                .each(function(colIdx) {
+                                    // Set the header cell to contain the input element
+                                    var cell = $('.filters th').eq(
+                                        $(api.column(colIdx).header()).index()
+                                    );
+                                    var title = $(cell).text();
+                                    $(cell).html('<input type="text" placeholder="' + title + '" />');
+
+                                    // On every keypress in this input
+                                    $(
+                                            'input',
+                                            $('.filters th').eq($(api.column(colIdx).header()).index())
+                                        )
+                                        .off('keyup change')
+                                        .on('change', function(e) {
+                                            // Get the search value
+                                            $(this).attr('title', $(this).val());
+                                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
+
+                                            var cursorPosition = this.selectionStart;
+                                            // Search the column for that value
+                                            api
+                                                .column(colIdx)
+                                                .search(
+                                                    this.value != '' ?
+                                                    regexr.replace('{search}', '(((' + this.value + ')))') :
+                                                    '',
+                                                    this.value != '',
+                                                    this.value == ''
+                                                )
+                                                .draw();
+                                        })
+                                        .on('keyup', function(e) {
+                                            e.stopPropagation();
+
+                                            $(this).trigger('change');
+                                            $(this)
+                                                .focus()[0]
+                                                .setSelectionRange(cursorPosition, cursorPosition);
+                                        });
+                                });
                         },
                     });
                 });
